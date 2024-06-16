8dd6d166ab4c6aa0c08deaea20520be41c916be4
anks117 <study.ankit117@gmail.com>
2024-06-16 15:31:33 +0530

    created cart model and cart routes and modified backend routes

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import { authenticate } from '../middleware/authMiddleware.js';
+import Cart from '../models/cartModel.js';
+
+const router=express.Router();
+
+router.post('/',authenticate,async(req,res)=>{
+    const {userid,productid}=req.body;
+    const productExisted=await Cart.findOne({user:userid,product:productid});
+
+    if(productExisted){
+        productExisted.quantity +=1;
+        productExisted.save();
+        return res.status(200).json(productExisted);
+    }else{
+        try {
+            const newCartProd=await Cart.create({
+                product:productid,
+                user:userid,
+                quantity:1
+            })
+            res.status(200).json(newCartProd);
+        } catch (error) {
+            res.status(400).json(error)
+        }
+    }
+})
+
+router.get('/:userid',authenticate,async(req,res)=>{
+    const userid=req.params.userid;
+    try {
+        const allInfoCartProducts=await Cart.find({user:userid}).populate('product');
+        const cartProducts = allInfoCartProducts.map((acp) => ({
+            _id: acp._id,
+            quantity: acp.quantity,
+            product: {
+                _id: acp.product._id,
+                name: acp.product.name,
+                image: acp.product.image,
+                brand: acp.product.brand,
+                description: acp.product.description
+            }
+        }));
+        res.status(200).json(cartProducts)
+    } catch (error) {
+        res.status(400).json({err:error});
+    }
+})
+
+router.delete('/:cpid',authenticate,async(req,res)=>{
+    const cpid=req.params.cpid;
+    const cpExist=await Cart.findById(cpid);
+
+    if(cpExist){
+        const qty=cpExist.quantity;
+        if(qty>1){
+            try {
+                cpExist.quantity -=1;
+                cpExist.save();
+                return res.status(200).json(cpExist);
+            } catch (error) {
+                res.status(400).json({msg:'not able to dec qty'});
+            }
+        }else{
+            try {
+                await Cart.findByIdAndDelete(cpid);
+                return res.status(200).json({msg:'product removed from cart'});
+            } catch (error) {
+                res.status(400).json({msg:'not able to remove'});
+            }
+        }
+    }
+
+    return res.status(400).json({msg:'already not present'});
+})
+
+export default router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ import userRouter from './userRoutes.js'
 import categoryRouter from './categoryRoutes.js'
 import productRouter from './productRoutes.js'
 import favouriteRouter from './favouriteRoutes.js'
+import cartRouter from './cartRoutes.js'
 
 const router=express.Router();
 
@@ -10,5 +11,6 @@ router.use('/user',userRouter);
 router.use('/category',categoryRouter);
 router.use('/product',productRouter);
 router.use('/favourite',favouriteRouter);
+router.use('/cart',cartRouter);
 
 export default router
