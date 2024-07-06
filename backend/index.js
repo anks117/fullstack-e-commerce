a223e2fa8f96e66c30fc746bca25e79375123e92
anks117 <study.ankit117@gmail.com>
2024-07-06 16:50:32 +0530

    modified cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
-app.use(cors({}))
+app.use(cors({
+    origin: '*',  // Allow all origins
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true  // Allow cookies and other credentials
+}))
 dotenv.config();
 
 
